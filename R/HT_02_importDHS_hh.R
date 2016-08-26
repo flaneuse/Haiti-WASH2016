3a87f84f8a359eebda7b082462a792587ffd94d1
flaneuse <laura.d.hughes@gmail.com>
2016-08-26 12:13:00 -0400

    migrating water, toilet code to separate file

--- R/HT_02_importDHS_hh.R
+++ R/HT_02_importDHS_hh.R
@@ -37,6 +37,27 @@ pullAttributes <- function(data) {
   return(df)
 }
 
+# Remove attribute information from object
+removeAttributes <- function(data) {
+  data <- lapply(data, function(x) {
+    attr(x, "labels") <- NULL
+    x
+  })
+  data <- lapply(data, function(x) {
+    attr(x, "label") <- NULL
+    x
+  })
+  data <- lapply(data, function(x) {
+    attr(x, "class") <- NULL
+    x
+  })
+  data <- lapply(data, function(x) {
+    attr(x, "levels") <- NULL
+    x
+  })
+  data = data.frame(data)
+}
+
 
 # Import data -------------------------------------------------------------
 
@@ -73,7 +94,8 @@ hh = hh %>%
          hh_num = hv002,
          sample_wt = hv005,
          prim_sampling_unit = hv021, # Usually same as cluster number and/or ultimate area unit, but may differ if multistage sampling used
-         sample_strata = hv022, # Groupings of primary sampling units for clustering errors by region
+         sample_strata22 = hv022, # Groupings of primary sampling units for clustering errors by region
+         sample_strata23 = hv023, # Groupings of primary sampling units for clustering errors by region
          urban = hv025,
          region = shregnew, # Note: using shregnew, not hv024, since it breaks Ouest into Aire Metropolitaine et Reste-Ouest
          interview_month = hv006,
@@ -109,6 +131,12 @@ hh = hh %>%
 # - Translate codes to strings
 # - Remove coded NA values
 # - Divide numbers by their scaling factor, since DHS hates decimal points.
+
+# NOTE: DHS treats NA values (missing, no response, didn't know, etc.) as a third category in most cases.
+# In this analysis, all those values are converted to NA and *removed* from the analysis.
+# Since you have no information on whether the variable should be one thing or another, I believe they
+# should be ignored to provide a more realistic estimate of the value, based on the available data.
+# Numbers may therefore be slightly different than the DHS-published numbers.
 
 region_codes = data.frame(code = attr(hh_raw$shregnew, 'labels')) 
 region_codes = region_codes %>% 
@@ -162,143 +190,11 @@ hh = hh %>%
 # hh %>% group_by(hv238, num_share_toilet) %>% summarise(n())
 
 
-# classify improved/ not improved -----------------------------------------
-# DHS claims to use WHO definitions for sanitation; similar to those provided by Dr. Elizabeth Jordan, 
-# WHO/UNICEF Joint Monitoring Programme for Water Supply and Sanitation: http://www.wssinfo.org/definitions-methods/watsan-categories/
-
-# NOTE! DHS classifies bottled water (eau en bouteille, petit vendeur d'eau) as being improved.
-# HOWEVER-- both WHO (JMP) and USAID WASH experts default to assuming the bottled water is unimproved unless proven otherwise.
-# If the bottled water is consumed out of preference and the household has access to an improved water source for cooking and 
-# personal hygiene, the bottled water can be deemed "improved." In the absence of that issue, it's assumed to be unimproved.
-# Unfortunately, the DHS didn't collect info on water sources for non-drinking water.
-# Therefore, we are going AGAINST the DHS report and classifying bottled water as unimproved.
-
-
-# -- WATER --
-# Export type of water sources in survey.
-water_types = attr(hh_raw$hv201, 'labels')
-write.csv(water_types, '~/GitHub/Haiti-WASH2016/dataout/DHS_water_types.csv')
-
-# read in Liz Jordan's classification of whether or not a toilet or water source is improved (see below in comments)
-water_types = read.csv('~/GitHub/Haiti-WASH2016/dataout/DHS_water_classification.csv')
-impr_water_codes = unlist(water_types %>% filter(improved == 1) %>% select(code))
-unimpr_water_codes = unlist(water_types %>% filter(improved == 0) %>% select(code))
-
-# Water sources are defined as being 'improved' if they are one of the following types and can be reached in <= 30 minutes
-#                                           watersource code improved
-#                                         piped water   10        1
-#                                 piped into dwelling   11        1
-#                                  piped to yard/plot   12        1
-#                             piped from the neighbor   13        1
-#                                public tap/standpipe   14        1
-#                                     tube well water   20        1
-#                               tube well or borehole   21        1
-#                             protected well  to yard   31        1
-#                               others protected well   32        1
-#                                    protected spring   41        1
-#                                           rainwater   51        1
-#                           dug well (open/protected)   30        0
-#                            unprotected well to yard   33        0
-#                  public and others unprotected well   34        0
-#                                       surface water   40        0
-#                                  unprotected spring   42        0
-# river/dam/lake/ponds/stream/canal/irrigation channel   43        0
-#                                        tanker truck   61        0
-#                                cart with small tank   62        0
-#                                       bottled water   71        0
-#                              sales company of water   72        0
-#                                               other   96        0
-
-hh = hh %>% 
-  mutate(
-    # -- straight classification of whether the source is improved --
-    improved_water = ifelse(water_source %in% impr_water_codes, 1,
-                            ifelse(water_source %in% unimpr_water_codes, 0, NA)),
-    # -- improved source + <= 30 min. to acquire --
-    # time2water = 996 == "on premises"; assumed to be < 30 min.
-    impr_water_under30min = ifelse(is.na(time2water) | is.na(improved_water), NA,
-                                   ifelse(time2water <= 30  & improved_water == 1, 1, 0))
-  )
-
-# -- TOILETS --
-# export types of toilets in survey
-toilet_types = attr(hh_raw$hv205, 'labels')
-write.csv(toilet_types, '~/GitHub/Haiti-WASH2016/dataout/DHS_toilet_types.csv')
-
-# read in Liz Jordan's classification of whether or not a toilet or water source is improved (see below in comments)
-toilet_types = read.csv('~/GitHub/Haiti-WASH2016/dataout/DHS_toilet_classification.csv')
-impr_toilet_codes = unlist(toilet_types %>% filter(improved == 'Improved') %>% select(code))
-unimpr_toilet_codes = unlist(toilet_types %>% filter(improved == 'Unimproved') %>% select(code))
-od_codes =  unlist(toilet_types %>% filter(improved == 'open defecation') %>% select(code))
-
-# Toilets are defined as being 'improved' if they are one of the following types and aren't shared.
-# Note: DHS defines mobile chemical toilets as improved (presumably b/c they treat the waste)
-# Only 13 hh outside the camps use them.
-
-#                           toilet_type code        improved
-#                         flush toilet   10        Improved
-#          flush to piped sewer system   11        Improved
-#                 flush to septic tank   12        Improved
-#                 flush to pit latrine   13        Improved
-#              flush to somewhere else   14        Improved
-#              flush, don't know where   15        Improved
-#                   pit toilet latrine   20        Improved
-# ventilated improved pit latrine (vip)   21        Improved
-#                pit latrine with slab   22        Improved
-#                    composting toilet   41        Improved
-#                          no facility   30 open defecation
-#               no facility/bush/field   31 open defecation
-#    pit latrine without slab/open pit   23      Unimproved
-#                        bucket toilet   42      Unimproved
-#               hanging toilet/latrine   43      Unimproved
-#           toilet hanging (on stilts)   44      Unimproved
-#               mobile chemical toilet   45      Unimproved
-#                                other   96      Unimproved
-
-
-# Checking that DHS variable "share_toilet" is correct binary for whether only one household uses toilet:
-# hh %>% group_by(share_toilet, num_share_toilet) %>% summarise(n())
-
-hh = hh %>% 
-  mutate(
-    # -- straight classification of whether the source is improved --
-    toilet_type = case_when(hh$toilet_source %in% impr_toilet_codes ~ 'improved',
-                            hh$toilet_source %in% unimpr_toilet_codes ~ 'unimproved',
-                            hh$toilet_source %in% od_codes ~ 'open defecation',
-                            TRUE ~ NA_character_))
-
-hh = hh %>% 
-  mutate(
-    # -- improved source + unshared --
-    improved_toilet = case_when(is.na(hh$toilet_type) ~ NA_real_,
-                                (hh$toilet_type == 'improved' & hh$share_toilet == 0) ~ 1, # improved, unshared
-                                (hh$toilet_type == 'improved' & hh$share_toilet == 1) ~ 0, # improved, shared
-                                hh$toilet_type %in% c('unimproved', 'open defecation') ~ 0, # unimproved or open defecation
-                                TRUE ~ NA_real_),
-    impr_toilet_type = case_when(is.na(hh$toilet_type) ~ NA_character_,
-                                 (hh$toilet_type == 'improved' & hh$share_toilet == 0) ~ 'improved-unshared', # improved, unshared
-                                 (hh$toilet_type == 'improved' & hh$share_toilet == 1) ~ 'improved-shared', # improved, shared
-                                 hh$toilet_type == 'unimproved' ~ 'unimproved', # unimproved
-                                 hh$toilet_type == 'open defecation' ~ 'open defecation', # open defecation
-                                 TRUE ~ NA_character_)
-  )
-
-# -- Quick summary tables --
-hh %>% group_by(toilet_type, improved_toilet) %>% summarise(n = n()) %>% ungroup() %>% mutate(pct = percent(n/sum(n), ndigits = 1))
-
-hh %>% group_by(impr_toilet_type) %>% summarise(n = n()) %>%  mutate(pct = percent(n/sum(n), ndigits = 1))
-
-hh %>% group_by(region_name, improved_toilet) %>% summarise(n = n()) %>% ungroup() %>%  group_by(region_name) %>% mutate(pct = n/sum(n)) %>% filter(improved_toilet == 1) %>% ungroup() %>% arrange(desc(pct))
-
-
 
 # Set up sampling weights --------------------------------------------------
 
-DHSdesign = svydesign(id = ~prim_sampling_unit, strata = ~sample_strata, weights = ~sample_wt, data = hh)
+DHSdesign = svydesign(id = ~prim_sampling_unit, strata = ~sample_strata22, weights = ~sample_wt, data = hh)
 summary(DHSdesign)
-
-svymean(~improved_toilet, DHSdesign, na.rm = TRUE)
-svymean(~improved_water, DHSdesign, na.rm = TRUE)
 
 
 # Merge in geocoordinates -----------------------------------------------------
